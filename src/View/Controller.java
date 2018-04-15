69dd9606f0ee1fc5f8f955bf9aaf92d491fb8c58
Justin Cruz <32960576+jcruz9678@users.noreply.github.com>
2018-04-15 19:18:04 -0400

    Add files via upload

--- src/View/Controller.java
+++ src/View/Controller.java
@@ -1,4 +1,195 @@
 package View;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+
 public class Controller {
+
+    //Login Page
+    public void loginButtonAction(ActionEvent event) {
+        //Button was clicked, login if username/password is correct
+        //TODO: Check database for existing user
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Input.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Input");
+            stage.setScene(new Scene(root, 1236, 769));
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void cancelLoginButtonAction(ActionEvent event) {
+        //On button click, close application
+        Platform.exit();
+    }
+
+    public void registerLoginButtonAction(ActionEvent event) {
+        //Button was clicked, open registration page
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Register.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Register");
+            stage.setScene(new Scene(root, 562, 370));
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //Register Page
+    public void cancelRegisterButtonAction(ActionEvent event) {
+        //Button was clicked, cancel registration
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(new Scene(root, 595, 422));
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void registerAction(ActionEvent event) {
+        //Button was clicked, Register new user
+        //TODO addd new user to database/check if user already exists
+    }
+
+
+    //Input Page
+    public void loadProfileInputButtonAction(ActionEvent event) {
+        //Button was clicked, go to profile page
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Profiles.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Profiles");
+            stage.setScene(new Scene(root, 567, 635));
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void logoutInputButtonAction(ActionEvent event) {
+        //On button click, log user out and return to login page
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(new Scene(root, 595, 422));
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void calcInputButtonAction(ActionEvent event) {
+        //On button click, perform calculations and go to Output page
+
+        //TODO: Perform calculations
+
+        //go to Output page
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Output.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Output");
+            stage.setScene(new Scene(root, 1236, 769));
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //Output Page
+    public void backOutputButtonAction(ActionEvent event) {
+        //On button click, return to Input page
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Input.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Input");
+            stage.setScene(new Scene(root, 1236, 769));
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveprofileOutputButtonAction(ActionEvent event) {
+        //On button click, Save the users results
+        //TODO: Implement this function
+    }
+
+    public void logoutOutputButtonAction(ActionEvent event) {
+        //On button click, log user out and return to login page
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(new Scene(root, 595, 422));
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //Profiles Page
+    public void loadProfilesButtonAction(ActionEvent event) {
+        //On button click, load the selected profile and populate with their data
+        //TODO: implement this function
+    }
+
+    public void cancelProfilesButtonAction(ActionEvent event) {
+        //On button click, return to input page
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Input.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Input");
+            stage.setScene(new Scene(root, 1236, 769));
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
--- src/View/Main.java
+++ src/View/Main.java
@@ -12,14 +12,18 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+
+        Scene scene = new Scene(root, 595, 422);
+
         primaryStage.setTitle("Login");
-        primaryStage.setScene(new Scene(root, 595, 422));
+        primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
+
