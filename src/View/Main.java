494f275d1f93d97c9fcb8075dee135c2d9a74f2a
Justin Cruz <32960576+jcruz9678@users.noreply.github.com>
2018-04-15 16:02:26 -0400

    Add files via upload

---
+++ src/View/Main.java
@@ -0,0 +1,25 @@
+package View;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 595, 422));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
